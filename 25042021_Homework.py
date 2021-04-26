6d03be30933e0413a9c821990080be4d3fe6853a
MartinaVincheva <76917343+MartinaVincheva@users.noreply.github.com>
2021-04-26 12:32:06 +0300

    Homework

---
+++ 25042021_Homework.py
@@ -0,0 +1,12 @@
+'''
+Created on 25.04.2021 
+
+@author: Marti
+'''
+
+def sum_of_two(first,second):
+    result = first + second
+    print(result)
+    
+sum_of_two(1, 2)
+sum_of_two("This is"," string")
